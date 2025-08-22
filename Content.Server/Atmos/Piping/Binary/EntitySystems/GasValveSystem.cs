9f09c6805dd57168316bea180c51effae7224e58
qwerltaz <69696513+qwerltaz@users.noreply.github.com>
2025-08-22 21:40:21 +0200

    atmos pump sound scales with pressure

--- Content.Server/Atmos/Piping/Binary/EntitySystems/GasValveSystem.cs
+++ Content.Server/Atmos/Piping/Binary/EntitySystems/GasValveSystem.cs
@@ -3,6 +3,7 @@ using Content.Server.NodeContainer.Nodes;
 using Content.Shared.Atmos.Piping.Binary.Components;
 using Content.Shared.Atmos.Piping.Binary.Systems;
 using Content.Shared.Audio;
+using Robust.Shared.Audio.Systems;
 
 namespace Content.Server.Atmos.Piping.Binary.EntitySystems;
 
@@ -10,6 +11,30 @@ public sealed class GasValveSystem : SharedGasValveSystem
 {
     [Dependency] private readonly SharedAmbientSoundSystem _ambientSoundSystem = default!;
     [Dependency] private readonly NodeContainerSystem _nodeContainer = default!;
+
+    public override void Update(float frameTime)
+    {
+        base.Update(frameTime);
+
+        var gasValveQuery = EntityQueryEnumerator<GasValveComponent>();
+        while (gasValveQuery.MoveNext(out var uid, out var component))
+        {
+            if (!_nodeContainer.TryGetNodes(uid,
+                    component.InletName,
+                    component.OutletName,
+                    out PipeNode? inlet,
+                    out PipeNode? outlet))
+                continue;
+
+            if (!component.Open)
+                continue;
+
+            var pressure = inlet.Air.Pressure;
+            var volume = MathF.Asinh(pressure);
+            _ambientSoundSystem.SetVolume(uid, volume);
+
+        }
+    }
 
     public override void Set(EntityUid uid, GasValveComponent component, bool value)
     {
@@ -22,6 +47,11 @@ public sealed class GasValveSystem : SharedGasValveSystem
                 inlet.AddAlwaysReachable(outlet);
                 outlet.AddAlwaysReachable(inlet);
                 _ambientSoundSystem.SetAmbience(uid, true);
+
+                var pressure = inlet.Air.Pressure;
+                var volume = MathF.Asinh(pressure);
+
+                _ambientSoundSystem.SetVolume(uid, volume);
             }
             else
             {
