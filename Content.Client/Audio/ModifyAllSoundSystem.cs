f6e7d1147bd914c99967ea8b93fbcdd85985b16e
qwerltaz <69696513+qwerltaz@users.noreply.github.com>
2025-12-12 21:10:20 +0100

    modify all sounds status effect

---
+++ Content.Client/Audio/ModifyAllSoundSystem.cs
@@ -0,0 +1,46 @@
+﻿using Content.Shared.Audio;
+using Content.Shared.StatusEffectNew;
+using Robust.Client.Audio;
+using Robust.Shared.Audio;
+using Robust.Shared.Audio.Components;
+
+namespace Content.Client.Audio;
+
+public sealed class ModifyAllSoundSystem : EntitySystem
+{
+    [Dependency] private readonly AudioSystem _audioSystem = default!;
+
+    private bool _enabled;
+    private AudioParams? _params;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<ModifyAllSoundStatusEffectComponent, StatusEffectAppliedEvent>(OnModifyAudioParamsApply);
+        SubscribeLocalEvent<ModifyAllSoundStatusEffectComponent, StatusEffectRemovedEvent>(OnModifyAudioParamsShutdown);
+        SubscribeLocalEvent<AudioComponent, ComponentInit>(OnAudioComponentInit);
+    }
+
+    private void OnModifyAudioParamsApply(Entity<ModifyAllSoundStatusEffectComponent> ent,
+        ref StatusEffectAppliedEvent args)
+    {
+        _enabled = true;
+        _params = ent.Comp.ModifiedAudioParams;
+    }
+
+    private void OnModifyAudioParamsShutdown(Entity<ModifyAllSoundStatusEffectComponent> ent,
+        ref StatusEffectRemovedEvent args)
+    {
+        _enabled = false;
+        _params = null;
+    }
+
+    private void OnAudioComponentInit(Entity<AudioComponent> ent, ref ComponentInit args)
+    {
+        if (!_enabled || _params is null)
+            return;
+
+        ent.Comp.Params = _params.Value;
+    }
+}
---
+++ Content.Shared/Audio/ModifyAllSoundStatusEffectComponent.cs
@@ -0,0 +1,14 @@
+﻿using Robust.Shared.Audio;
+using Robust.Shared.GameStates;
+
+namespace Content.Shared.Audio;
+
+/// <summary>
+///
+/// </summary>
+[RegisterComponent, NetworkedComponent]
+public sealed partial class ModifyAllSoundStatusEffectComponent : Component
+{
+    [DataField]
+    public AudioParams ModifiedAudioParams = AudioParams.Default.WithPitchScale(2.0f);
+}
