59bb782585123f04f10f89ad5e3bc485e8861985
qwerltaz <69696513+qwerltaz@users.noreply.github.com>
2026-02-19 20:26:24 +0100

    toggle on player detach and only on local player

--- Content.Client/Audio/ModifyAllSoundSystem.cs
+++ Content.Client/Audio/ModifyAllSoundSystem.cs
@@ -1,12 +1,16 @@
 ﻿using Content.Shared.Audio;
 using Content.Shared.StatusEffectNew;
 using Robust.Client.Audio;
+using Robust.Client.Player;
 using Robust.Shared.Audio.Components;
+using Robust.Shared.Player;
 
 namespace Content.Client.Audio;
 
 public sealed class ModifyAllSoundSystem : EntitySystem
 {
+    [Dependency] private readonly IPlayerManager _player = default!;
+
     private bool _enabled;
     private float _volumeMultiplier = 1f;
     private float _pitchMultiplier = 1f;
@@ -19,6 +23,10 @@ public sealed class ModifyAllSoundSystem : EntitySystem
 
         SubscribeLocalEvent<ModifyAllSoundStatusEffectComponent, StatusEffectAppliedEvent>(OnModifyAudioParamsApply);
         SubscribeLocalEvent<ModifyAllSoundStatusEffectComponent, StatusEffectRemovedEvent>(OnModifyAudioParamsShutdown);
+        SubscribeLocalEvent<ModifyAllSoundStatusEffectComponent, StatusEffectRelayedEvent<LocalPlayerAttachedEvent>>(
+            OnPlayerAttached);
+        SubscribeLocalEvent<ModifyAllSoundStatusEffectComponent, StatusEffectRelayedEvent<LocalPlayerDetachedEvent>>(
+            OnPlayerDetached);
     }
 
     public override void FrameUpdate(float frameTime)
@@ -40,6 +48,9 @@ public sealed class ModifyAllSoundSystem : EntitySystem
     private void OnModifyAudioParamsApply(Entity<ModifyAllSoundStatusEffectComponent> ent,
         ref StatusEffectAppliedEvent args)
     {
+        if (_player.LocalEntity != args.Target)
+            return;
+
         _enabled = true;
         _volumeMultiplier = ent.Comp.Volume;
         _pitchMultiplier = ent.Comp.Pitch;
@@ -47,6 +58,21 @@ public sealed class ModifyAllSoundSystem : EntitySystem
 
     private void OnModifyAudioParamsShutdown(Entity<ModifyAllSoundStatusEffectComponent> ent,
         ref StatusEffectRemovedEvent args)
+    {
+        if (_player.LocalEntity != args.Target)
+            return;
+
+        _enabled = false;
+    }
+
+    private void OnPlayerAttached(Entity<ModifyAllSoundStatusEffectComponent> ent,
+        ref StatusEffectRelayedEvent<LocalPlayerAttachedEvent> args)
+    {
+        _enabled = true;
+    }
+
+    private void OnPlayerDetached(Entity<ModifyAllSoundStatusEffectComponent> ent,
+        ref StatusEffectRelayedEvent<LocalPlayerDetachedEvent> args)
     {
         _enabled = false;
     }
