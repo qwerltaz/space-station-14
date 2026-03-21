b5b8722b71621c8717a798dbef626711a909aced
qwerltaz <69696513+qwerltaz@users.noreply.github.com>
2026-03-21 12:31:34 +0100

    split waitAssertion

--- Content.IntegrationTests/Tests/Interaction/MultiHandItemTest.cs
+++ Content.IntegrationTests/Tests/Interaction/MultiHandItemTest.cs
@@ -1,5 +1,9 @@
-﻿using Content.Shared.Hands.Components;
+﻿using System.Threading.Tasks;
+using Content.Shared.Hands.Components;
+using Content.Shared.Item;
+using NUnit.Framework;
 using Robust.Shared.GameObjects;
+using Robust.UnitTesting.Pool;
 
 namespace Content.IntegrationTests.Tests.Interaction;
 
@@ -32,24 +36,39 @@ public sealed class MultiHandItemTest : InteractionTest
         """;
 
     [Test]
+    [TestOf(typeof(MultiHandedItemComponent))]
+    [Description(
+        "Ensures that multi-handed items properly block hands from picking up other items, and that they require enough hands to pick up.")]
     public async Task BlockedHandsTest()
     {
         await Server.WaitAssertion(() =>
         {
             var item1 = SEntMan.SpawnEntity(TestMultiHandItemPrototypeId, SEntMan.GetCoordinates(TargetCoords));
+
             Assert.That(!HandSys.TryPickupAnyHand(SPlayer, item1),
                 "Picked up a multi-handed item while having only one hand.");
+        });
 
+        await Server.WaitAssertion(() =>
+        {
             HandSys.AddHand(SPlayer, HandLeft, HandLocation.Left);
             var item2 = SEntMan.SpawnEntity(TestMultiHandItemPrototypeId, SEntMan.GetCoordinates(TargetCoords));
-            Assert.That(HandSys.TryPickup(SPlayer, item2, HandLeft),
-                "Could not pick up a two-handed item with two free hands.");
 
-            Assert.That(HandSys.CountFreeHands(SPlayer),
-                Is.Zero,
-                "Free hands remained after using all hands to pick up a multi-handed item.");
+            Assert.Multiple(() =>
+            {
+                Assert.That(HandSys.TryPickup(SPlayer, item2, HandLeft),
+                    "Could not pick up a two-handed item with two free hands.");
 
+                Assert.That(HandSys.CountFreeHands(SPlayer),
+                    Is.Zero,
+                    "Free hands remained after using all hands to pick up a multi-handed item.");
+            });
+        });
+
+        await Server.WaitAssertion(() =>
+        {
             var item3 = SEntMan.SpawnEntity(TestItemPrototypeId, SEntMan.GetCoordinates(TargetCoords));
+
             Assert.That(!HandSys.TryPickup(SPlayer, item3, HandRight),
                 "Picked up an item with a hand blocked by a multi-handed item.");
         });
